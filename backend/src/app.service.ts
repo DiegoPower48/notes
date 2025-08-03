df076335be103c6ca8c4bf01fae1a501c2c9446c
Diego <diego_torres_11@hotmail.com>
2025-08-03 08:26:48 -0500

    probando

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('PORT:', process.env.PORT);
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
+    console.log('JWT_TIME:', process.env.JWT_TIME);
     return 'Hello World!';
   }
 }
